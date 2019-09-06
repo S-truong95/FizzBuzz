caf9f7664ce6d51fc259de0fda05f4455cdeb1d7
Steven <steventruong1995@yahoo.com>
2019-09-06 15:00:15 -0400

    commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Get(int num)
+        {
+            if (IsMultipleOf3(num))
+            {
+                return "Fizz";
+            }
+            if (IsMultipleOf5(num))
+            {
+                return "Buzz";
+            }
+            return num.ToString();
+        }
+        private bool IsMultipleOf3(int numberToCheck)
+        {
+            return numberToCheck % 3 == 0;
+        }
+        private bool IsMultipleOf5(int numberToCheck)
+        {
+            return numberToCheck % 5 == 0;
+        }
+    }
+}
+    
+
